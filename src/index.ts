50c7641aaf711bd1717d497a0a5dcee1a41b2158
WebDevNerdStuff <webdevnerdstuff@gmail.com>
2023-03-16 13:40:33 -0700

    Updating types

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { App, Plugin } from 'vue';
 import { CodeBlock } from '@/plugin';
-import { OptionsSettings } from '@/types';
+import { Props } from '@/types';
 import '@/plugin/styles/cssVariables.css';
 
 const CodeBlockPlugin = {
-	install(app: App, options: OptionsSettings) {
-		const codeBlockGlobalOptions = { globalOptions: true, ...options };
+	install(app: App, options: Props) {
+		const codeBlockGlobalOptions: Props = { globalOptions: true, ...options };
 
 		app.provide('codeBlockGlobalOptions', codeBlockGlobalOptions);
 		app.component('CodeBlock', CodeBlock);
