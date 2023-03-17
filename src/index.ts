b816ff37c0f9bc1c1bd0931cb7cf739ef7494127
WebDevNerdStuff <webdevnerdstuff@gmail.com>
2023-03-16 19:15:04 -0700

    Styles fun some more

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { App, Plugin } from 'vue';
 import { CodeBlock } from '@/plugin';
 import { Props } from '@/types';
+import '@/plugin/styles/cssVariables.css';
+import '@/plugin/styles/utilities.scss';
+import '@/plugin/styles/main.scss';
+import '@/plugin/styles/themeStyles.scss';
 
 const install = (app: App, options: Props) => {
 	const codeBlockGlobalOptions: Props = { globalOptions: true, ...options };
