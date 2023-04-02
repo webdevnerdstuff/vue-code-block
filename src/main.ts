f11b210f3f81fa0d6cadf3ad340b64ec8f23e217
WebDevNerdStuff <webdevnerdstuff@gmail.com>
2023-04-01 19:21:56 -0700

    Adding icons

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,23 @@ import App from './App.vue';
 import CodeBlock from './index';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faCopyright, faHouse } from '@fortawesome/free-solid-svg-icons';
+import {
+	faCircleInfo,
+	faCopyright,
+	faHouse,
+	faMoon,
+	faSun,
+} from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faNpm } from '@fortawesome/free-brands-svg-icons';
 
 library.add({
+	faCircleInfo,
 	faCopyright,
 	faGithub,
 	faHouse,
+	faMoon,
 	faNpm,
+	faSun,
 });
 
 createApp(App)
