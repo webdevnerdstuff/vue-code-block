f912a5fb779f51ba91681934b2668630122ba651
WebDevNerdStuff <webdevnerdstuff@gmail.com>
2023-04-09 15:41:08 -0700

    Added pinia and updated accordingly

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 
 <script setup lang="ts">
 import { ref, provide } from 'vue';
-import { dependencies, name } from '../package';
+import { useCoreStore } from '@/stores/index';
 import DemoPage from '@/components/DemoPage.vue';
 import NavBar from '@/components/Layout/NavBar.vue';
 import FooterBar from '@/components/Layout/FooterBar.vue';
@@ -20,41 +20,12 @@ provide('styleData', {
 	},
 });
 
-const packageName = name;
-const repoBaseUrl = `https://github.com/webdevnerdstuff/${packageName}`;
-const prismVersion = dependencies.prismjs.replace('^', '');
-const highlightJsVersion = dependencies['highlight.js'].replace('^', '');
+const store = useCoreStore();
 const selectedLibrary = ref({});
 
-
-const links = {
-	changeLog: `${repoBaseUrl}/blob/main/CHANGELOG.md`,
-	contribute: `${repoBaseUrl}/tree/main/src/plugin/styles`,
-	github: repoBaseUrl,
-	githubProfile: 'https://github.com/webdevnerdstuff',
-	license: `${repoBaseUrl}/blob/main/LICENSE.md`,
-	neonBunnyTheme: 'https://marketplace.visualstudio.com/items?itemName=WebDevNerdStuff.neon-bunny',
-	npm: `https://www.npmjs.com/package/${packageName}`,
-	uaParser: 'https://www.npmjs.com/package/ua-parser-js',
-	vueJs: 'https://vuejs.org/',
-};
-
-const highlightJsLinks = {
-	cdn: `https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@${highlightJsVersion}/build/styles/`,
-	homepage: 'https://highlightjs.org/',
-	themes: `https://github.com/highlightjs/highlight.js/tree/${highlightJsVersion}/src/styles`,
-};
-
-const prismLinks = {
-	cdn: `https://cdn.jsdelivr.net/gh/PrismJS/prism@${prismVersion}/themes/`,
-	homepage: 'https://prismjs.com/',
-	prismThemes: `https://github.com/PrismJS/prism/tree/v${prismVersion}/themes`,
-	themes: 'https://github.com/PrismJS/prism-themes',
-};
-
-provide('links', links);
-provide('highlightJsLinks', highlightJsLinks);
-provide('prismLinks', prismLinks);
+provide('links', store.links);
+provide('highlightJsLinks', store.highlightJsLinks);
+provide('prismLinks', store.prismLinks);
 
 const codeBlockOptions = ref({
 	browserWindow: false,
--- src/components/DemoPage.vue
+++ src/components/DemoPage.vue
@@ -141,6 +141,7 @@ import {
 	reactive,
 	ref,
 } from 'vue';
+import { useCoreStore } from '@/stores/index';
 import { version } from '../../package';
 import {
 	ChangeLogSection,
@@ -165,372 +166,25 @@ import 'prismjs/components/prism-php';
 import 'prismjs/plugins/line-numbers/prism-line-numbers.js';
 import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
 
+const demoTestPage = ref(false);
 
 const highlightJsLinks = inject('highlightJsLinks');
 const prismLinks = inject('prismLinks');
 
-const demoTestPage = ref(false);
+const store = useCoreStore();
 const library = ref('prism');
-const storageName = ref('vue3-code-block-library');
+const libraries = store.libraries;
 
-const selectedTheme = ref('neon-bunny');
+const highlightThemes = store.highlightThemes;
+const neonBunnyThemes = store.neonBunnyThemes;
+const prismThemes = store.prismThemes;
 const selectOptions = ref(null);
-const libraries = {
-	prism: {
-		label: 'PrismJS',
-		id: 'prism',
-	},
-	highlightjs: {
-		label: 'Highlight.js',
-		id: 'highlightjs',
-	},
-};
 const selectedLibrary = ref(libraries.prism);
-const neonBunnyThemes = [
-	{
-		label: 'Neon Bunny',
-		value: 'neon-bunny',
-	},
-	{
-		label: 'Neon Bunny - Carrot',
-		value: 'neon-bunny-carrot',
-	},
-];
-const prismThemes = [
-	{
-		label: 'Default',
-		value: 'default',
-	},
-	{
-		label: 'Coy',
-		value: 'coy',
-	},
-	{
-		label: 'Dark',
-		value: 'dark',
-	},
-	{
-		label: 'Funky',
-		value: 'funky',
-	},
-	{
-		label: 'Night Owl (Prism Themes Repo)',
-		value: 'themes-night-owl',
-	},
-	{
-		label: 'Okaidia',
-		value: 'okaidia',
-	},
-	{
-		label: 'Solarizedlight',
-		value: 'solarizedlight',
-	},
-	{
-		label: 'Tomorrow',
-		value: 'tomorrow',
-	},
-	{
-		label: 'Twilight',
-		value: 'twilight',
-	},
-];
-const highlightThemes = [
-	{
-		label: 'Default',
-		value: 'default',
-	},
-	{
-		label: 'A11y Dark',
-		value: 'a11y-dark',
-	},
-	{
-		label: 'A11y Light',
-		value: 'a11y-light',
-	},
-	{
-		label: 'Agate',
-		value: 'agate',
-	},
-	{
-		label: 'An Old Hope',
-		value: 'an-old-hope',
-	},
-	{
-		label: 'Android Studio',
-		value: 'androidstudio',
-	},
-	{
-		label: 'Arduino Light',
-		value: 'arduino-light',
-	},
-	{
-		label: 'Arta',
-		value: 'arta',
-	},
-	{
-		label: 'Ascetic',
-		value: 'ascetic',
-	},
-	{
-		label: 'Atom One Dark Reasonable',
-		value: 'atom-one-dark-reasonable',
-	},
-	{
-		label: 'Atom One Dark',
-		value: 'atom-one-dark',
-	},
-	{
-		label: 'Atom One Light',
-		value: 'atom-one-light',
-	},
-	{
-		label: 'Brown Paper',
-		value: 'brown-paper',
-	},
-	{
-		label: 'Base16 One Dark',
-		value: 'base16-onedark',
-	},
-	{
-		label: 'CodePen Embed',
-		value: 'codepen-embed',
-	},
-	{
-		label: 'Color Brewer',
-		value: 'color-brewer',
-	},
-	{
-		label: 'Dark',
-		value: 'dark',
-	},
-	{
-		label: 'DeviBeans',
-		value: 'devibeans',
-	},
-	{
-		label: 'Docco',
-		value: 'docco',
-	},
-	{
-		label: 'Far',
-		value: 'far',
-	},
-	{
-		label: 'Felipec',
-		value: 'felipec',
-	},
-	{
-		label: 'Foundation',
-		value: 'foundation',
-	},
-	{
-		label: 'Github Dark Dimmed',
-		value: 'github-dark-dimmed',
-	},
-	{
-		label: 'Github Dark',
-		value: 'github-dark',
-	},
-	{
-		label: 'Github',
-		value: 'github',
-	},
-	{
-		label: 'GML',
-		value: 'gml',
-	},
-	{
-		label: 'Google Code',
-		value: 'googlecode',
-	},
-	{
-		label: 'Gradient Dark',
-		value: 'gradient-dark',
-	},
-	{
-		label: 'Gradient Light',
-		value: 'gradient-light',
-	},
-	{
-		label: 'Grayscale',
-		value: 'grayscale',
-	},
-	{
-		label: 'Hybrid',
-		value: 'hybrid',
-	},
-	{
-		label: 'Idea',
-		value: 'idea',
-	},
-	{
-		label: 'IntelliJ Light',
-		value: 'intellij-light',
-	},
-	{
-		label: 'IR Black',
-		value: 'ir-black',
-	},
-	{
-		label: 'ISBL Editor Dark',
-		value: 'isbl-editor-dark',
-	},
-	{
-		label: 'ISBL Editor Light',
-		value: 'isbl-editor-light',
-	},
-	{
-		label: 'Kimbie Dark',
-		value: 'kimbie-dark',
-	},
-	{
-		label: 'Kimbie Light',
-		value: 'kimbie-light',
-	},
-	{
-		label: 'Lightfair',
-		value: 'lightfair',
-	},
-	{
-		label: 'Lioshi',
-		value: 'lioshi',
-	},
-	{
-		label: 'Magula',
-		value: 'magula',
-	},
-	{
-		label: 'Mono Blue',
-		value: 'mono-blue',
-	},
-	{
-		label: 'Monokai Sublime',
-		value: 'monokai-sublime',
-	},
-	{
-		label: 'Monokai',
-		value: 'monokai',
-	},
-	{
-		label: 'Night Owl',
-		value: 'night-owl',
-	},
-	{
-		label: 'NNFX Dark',
-		value: 'nnfx-dark',
-	},
-	{
-		label: 'NNFX- Light',
-		value: 'nnfx-light',
-	},
-	{
-		label: 'Nord',
-		value: 'nord',
-	},
-	{
-		label: 'Obsidian',
-		value: 'obsidian',
-	},
-	{
-		label: 'Panda Syntax Dark',
-		value: 'panda-syntax-dark',
-	},
-	{
-		label: 'Panda Syntax Light',
-		value: 'panda-syntax-light',
-	},
-	{
-		label: 'Paraiso Dark',
-		value: 'paraiso-dark',
-	},
-	{
-		label: 'Paraiso Light',
-		value: 'paraiso-light',
-	},
-	{
-		label: 'Pojoaque',
-		value: 'pojoaque',
-	},
-	{
-		label: 'PureBasic',
-		value: 'purebasic',
-	},
-	{
-		label: 'Qt Creator Dark',
-		value: 'qtcreator-dark',
-	},
-	{
-		label: 'Qt Creator Light',
-		value: 'qtcreator-light',
-	},
-	{
-		label: 'Rainbow',
-		value: 'rainbow',
-	},
-	{
-		label: 'RouterOS',
-		value: 'routeros',
-	},
-	{
-		label: 'School Book',
-		value: 'school-book',
-	},
-	{
-		label: 'Shades of Purple',
-		value: 'shades-of-purple',
-	},
-	{
-		label: 'Srcery',
-		value: 'srcery',
-	},
-	{
-		label: 'Stack Overflow Dark',
-		value: 'stackoverflow-dark',
-	},
-	{
-		label: 'Stack Overflow Light',
-		value: 'stackoverflow-light',
-	},
-	{
-		label: 'Sunburst',
-		value: 'sunburst',
-	},
-	{
-		label: 'Tokyo Night Dark',
-		value: 'tokyo-night-dark',
-	},
-	{
-		label: 'Tokyo Night Light',
-		value: 'tokyo-night-light',
-	},
-	{
-		label: 'Tomorrow Night Blue',
-		value: 'tomorrow-night-blue',
-	},
-	{
-		label: 'Tomorrow Night Bright',
-		value: 'tomorrow-night-bright',
-	},
-	{
-		label: 'VS',
-		value: 'vs',
-	},
-	{
-		label: 'VS 2015',
-		value: 'vs2015',
-	},
-	{
-		label: 'Xcode',
-		value: 'xcode',
-	},
-	{
-		label: 'xt256',
-		value: 'xt256',
-	},
-];
+const selectedTheme = ref('neon-bunny');
+
 
 onMounted(() => {
-	library.value = getLocalStorage() ?? setLocalStorage();
+	library.value = store.getLocalStorage() ?? store.setLocalStorage();
 	changeLibrary(library.value);
 });
 
@@ -551,7 +205,7 @@ function changeLibrary(val) {
 	selectedLibrary.value = libraries[val];
 	selectedTheme.value = 'neon-bunny';
 
-	setLocalStorage(library.value);
+	store.setLocalStorage(library.value);
 	emit('changedLibrary', selectedLibrary);
 
 	if (val === 'prism') {
@@ -565,19 +219,6 @@ function changeLibrary(val) {
 function changeTheme(val) {
 	selectedTheme.value = val;
 }
-
-function getLocalStorage(): string {
-	const value = localStorage.getItem(storageName.value);
-	return value;
-}
-
-function setLocalStorage(val = 'prism'): string {
-	const oldValue = localStorage.getItem(storageName.value);
-	const newValue = val ?? oldValue;
-
-	localStorage.setItem(storageName.value, newValue);
-	return newValue;
-};
 </script>
 
 
