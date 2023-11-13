4dd0d8bfee273fa7527e0fb34af7bb79fa368f3e
WebDevNerdStuff <webdevnerdstuff@gmail.com>
2023-11-13 14:35:41 -0800

    Update example

--- src/documentation/components/examples/LangExamples.vue
+++ src/documentation/components/examples/LangExamples.vue
@@ -170,9 +170,6 @@ const htmlExample = `<!DOCTYPE html>
 	<meta property="og:image:width" content="1200" />
 	<meta property="og:image:height" content="630" />
 
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-		integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Henny+Penny&family=Indie+Flower&display=swap" rel="stylesheet">
@@ -183,9 +180,6 @@ const htmlExample = `<!DOCTYPE html>
 <body>
 	<div id="app"><\/div>
 	<script type="module" src="/src/main.ts"><\/script>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-		integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-		crossorigin="anonymous"><\/script>
 <\/body>
 
 <\/html>
