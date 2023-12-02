ac016e40ef8a4d3f905ff4152674a2b45b07768d
WebDevNerdStuff <webdevnerdstuff@gmail.com>
2023-12-01 18:06:43 -0800

    Build

--- dist/plugin/index.d.ts
+++ dist/plugin/index.d.ts
@@ -1,6 +1,6 @@
 import type { App } from 'vue';
 import type { Props } from './types';
-import VCodeBlock from '../plugin/VCodeBlock.vue';
+import VCodeBlock from './VCodeBlock.vue';
 export declare const codeBlockOptions: unique symbol;
 export declare function createVCodeBlock(options?: Props): (app: App) => void;
 export { VCodeBlock, };
